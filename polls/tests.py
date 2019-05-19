1164d69c6e4722d820980b3de44d5512b32e3689
llwf <775729053@qq.com>
2019-05-19 23:10:19 +0800

    complete polls app

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,30 @@
+import datetime
+from django.utils import timezone
 from django.test import TestCase
-
+from .models import Question
 # Create your tests here.
+
+class QuestionMethodTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """
+        在将来发布的问卷应该返回False
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        在超过一天发布的问卷应该返回False
+        """
+        time = timezone.now() - datetime.timedelta(days=30, seconds=1)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recently_question(self):
+        """
+        最近一天内发布的问卷，返回True
+        """
+        time = timezone.now() - datetime.timedelta(hours=22, seconds=12)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), True)
--- polls/urls.py
+++ polls/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 from django.urls import path
 from . import views
 
-
+app_name = 'polls'
 urlpatterns = [
     # /polls/
     path('', views.index, name='index'),
     # /polls/question_id?
-    path('<int:question_id>', views.detail, name='detail'),
+    path('<int:question_id>/', views.detail, name='detail'),
     # /polls/question_id?/results/
     path('<int:question_id>/results/', views.results, name='results'),
     # /polls/question_id?/vote/
